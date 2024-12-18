218317ee9f96c328e3477c4e4b273de3a24e239a
salonijuneja93 <salonijuneja93@gmail.com>
2024-12-18 14:30:33 -0800

    Delete book app-backend directory

--- book app-backend/models/Book.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const BookSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  status: { type: String, default: "Unread" },
-});
-
-module.exports = mongoose.model('Book', BookSchema);
