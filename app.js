f1ece945117a16a1fa1099a8120ee470a4622dd2
salonijuneja93 <salonijuneja93@gmail.com>
2024-12-18 14:32:33 -0800

    app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const mongoURI = process.env.MONGO_URI || "your_mongodb_connection_string";
+const mongoURI = process.env.MONGO_URI || "mongodb+srv://salonijuneja93:salonijuneja93@saloni07.6xpr2.mongodb.net/";
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("MongoDB connected"))
     .catch(err => console.error(err));
